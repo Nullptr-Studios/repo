0f557ba5a8fba26f3e6478e2b5541dda2801fe5c
Xein <xgonip@gmail.com>
2026-01-09 14:02:47 +0100

    VST repo

---
+++ packages/v/vst3sdk/xmake.lua
@@ -0,0 +1,17 @@
+package("vst3sdk", function()
+	set_kind("library")
+	set_homepage("https://steinbergmedia.github.io/vst3sdk/")
+	set_description("Steinberg VST3 SDK")
+
+	add_urls("https://github.com/steinbergmedia/vst3sdk/archive/refs/tags/$(version).tar.gz")
+	add_versions("3.8.0_build_66", "D93C12077D848977D4D397C8AB6721C4B4966223A7707BFE6519AA419C396AF4")
+
+	on_install(function (package)
+		os.cp("pluginterfaces", package:installdir("include"))
+		os.cp("public.sdk", package:installdir("include"))
+	end)
+
+	on_load(function (package)
+		package:add("defines", "SMTG_CREATE_PLUGIN_LINK")
+	end)
+end)
---
+++ packages/v/vstwebview/xmake.lua
@@ -0,0 +1,33 @@
+package("vstwebview", function()
+	set_kind("library")
+	set_homepage("https://github.com/rdaum/vstwebview")
+	set_description("VST3 WebView UI support")
+
+	add_urls("https://github.com/rdaum/vstwebview.git")
+	add_versions("main", "main")
+
+	add_deps("vst3sdk")
+
+	if is_plat("linux") then
+		add_deps("webkit2gtk")
+	end
+
+	on_install(function (package)
+		os.cp("include", package:installdir())
+		os.cp("src", package:installdir())
+	end)
+
+	on_load(function (package)
+		package:add("includedirs", "include")
+		package:add("srcs", "src/**.cpp")
+
+		if is_plat("windows") then
+			package:add("defines", "VSTWEBVIEW_USE_WEBVIEW2")
+			package:add("syslinks", "user32", "ole32")
+		elseif is_plat("macosx") then
+			package:add("frameworks", "WebKit", "Cocoa")
+		elseif is_plat("linux") then
+			package:add("defines", "VSTWEBVIEW_USE_WEBKITGTK")
+		end
+	end)
+end)
